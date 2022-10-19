c97eb61c6661bc6113225b2874a356328b4629df
algamhr <algamahargarika9@gmail.com>
2022-10-19 14:54:22 +0700

    Create val, var, and comment

---
+++ app/src/main/java/com/algatraining/trainee/basiccode/Basic.kt
@@ -0,0 +1,21 @@
+package com.algatraining.trainee.basiccode
+
+/**
+ * Create by Alga Mahargarika on 19/10/2022
+ */
+
+fun main(){
+    //immutable variable, val tidak bisa diubah (kekal)
+    val myName = "Alga"
+    println("Hello " + myName)
+
+    //mutable variable, var bisa diubah
+    var lastName = "Mahargarika"
+    lastName = "Train"
+    print("Hello " + lastName)
+
+    /*
+    This is multiline comment, untuk komentar lebih dari satu line
+     */
+    // This is line comment, untuk komentar hanya untuk satu line, tidak bisa lebih dari satu
+}
--- app/src/main/java/com/algatraining/trainee/basiccode/calculation.kt
+++
@@ -1,8 +0,0 @@
-package com.algatraining.trainee.basiccode
-
-/**
- * Create by Alga Mahargarika on 18/10/2022
- */
-class calculation {
-
-}
