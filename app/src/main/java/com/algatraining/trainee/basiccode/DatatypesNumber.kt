ca20abbfd22fe72c567e32974fc5c33c5bb969ab
algamhr <algamahargarika9@gmail.com>
2022-10-19 14:54:41 +0700

    Create Datatypes of Number

---
+++ app/src/main/java/com/algatraining/trainee/basiccode/DatatypesNumber.kt
@@ -0,0 +1,32 @@
+package com.algatraining.trainee.basiccode
+
+/**
+ * Create by Alga Mahargarika on 19/10/2022
+ */
+
+fun main(){
+    //types of string
+    val myName = "Alga"
+
+    //types of int
+    val myAge = 24
+
+    /*
+    Integer types: Byte (8 bit), Short (16 bit), Int (32 bit), Long (64 bit)
+     */
+    val myByte: Byte = 13
+    val myShort: Short = 256
+    val myInt: Int = 899217222
+    val myLong: Long = 123_231_231_231_231_231
+
+    /*
+    Floating Point Number Types: Float (32 bit), Double (64 bit).
+    Floating number adalah nilai berkoma.
+    Float harus menggunakan huruf F diakhir variabel.
+    Jika tidak ditambah huruf F diakhir, maka variabel dianggap menjadi Double.
+    Double maksimal ada di 15 angka setelah titik (.)
+     */
+
+    val myFloat: Float = 13.32F
+    val myDouble: Double = 3.123827429734122
+}
