0937cacfd61d22a4cf326f138a3d90a45d8853f7
algamhr <algamahargarika9@gmail.com>
2022-10-19 15:22:13 +0700

    Exercise Basic (Bab 2)

---
+++ app/src/main/java/com/algatraining/trainee/basiccode/Exercise.kt
@@ -0,0 +1,25 @@
+package com.algatraining.trainee.basiccode
+
+/**
+ * Create by Alga Mahargarika on 19/10/2022
+ */
+
+fun main() {
+    var myString: String = "Android Masterclass"
+    val myFloat: Float = 13.37F
+    var myDouble: Double = 3.14159265358979
+    var myByte: Byte = 25
+    var myInt: Int = 2020
+    var myLong: Long  = 188_812_345_67
+    var myBoolean: Boolean = true
+    var a: Char = 'a'
+
+    println(myString)
+    println(myFloat)
+    println(myDouble)
+    println(myByte)
+    println(myInt)
+    println(myLong)
+    println(myBoolean)
+    println(a)
+}
