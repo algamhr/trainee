fa2da759ed93079404d3add68d8e1bcb6c197348
algamhr <algamahargarika9@gmail.com>
2022-10-21 14:52:06 +0700

    Create Comparison Operators

---
+++ app/src/main/java/com/algatraining/trainee/basiccode/ComparisonOperators.kt
@@ -0,0 +1,20 @@
+package com.algatraining.trainee.basiccode
+
+/**
+ * Create by Alga Mahargarika on 21/10/2022
+ */
+
+fun main() {
+    /*
+    Comparison operators (==, !=, >, <, >=, <=)
+    Biasanya hasil yang dikeluarkan adalah true or false
+     */
+    val isEqual = 5==3
+
+    println("The Equal is $isEqual")
+
+    val isNotEqual = 5!=5
+    println("The Equal is $isNotEqual")
+    println("is5greater3 ${5>3}")
+    println("is-5greater3 ${-5>3}")
+}
