3c12b03e0b5d61e1533c013b1f97185d9d7ebfec
algamhr <algamahargarika9@gmail.com>
2022-10-21 14:52:33 +0700

    Create Assignment Operators, Increment & Decrement

---
+++ app/src/main/java/com/algatraining/trainee/basiccode/AssignmentIncrementDecrement.kt
@@ -0,0 +1,24 @@
+package com.algatraining.trainee.basiccode
+
+/**
+ * Create by Alga Mahargarika on 21/10/2022
+ */
+
+fun main() {
+    //Assignment operators (+=, -=, *=, /=, %=)
+    var myNum = 5
+    myNum += 3
+    println("myNum is $myNum")
+    myNum *= 4
+    println("myNum is $myNum")
+
+
+    //Increment & Decrement operators (++, --)
+    myNum++
+    println("myNum is $myNum")
+    // increments after use
+    println("myNum is ${myNum++}")
+    // increments before use
+    println("myNum is ${++myNum}")
+    println("myNum is ${--myNum}")
+}
